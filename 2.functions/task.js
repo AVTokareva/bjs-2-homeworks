e9c0e98c0b4265ea331fdccb4b47979ffeda0922
Alina Tokareva <SoldatenkovaAV@yandex.ru>
2022-10-20 13:34:47 +0300

    homework-2 is done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,58 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+    max = -Infinity;
+    min = Infinity;
+  sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++){ //перебираем массив, находим макс и мин, суммируем элементы массива
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  avg = Number((sum/arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
+
 // Задание 2
-function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+function worker(arr) { //считаем сумму элементов 1 массива
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
   return sum;
 }
-
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
-  return max;
+ 
+function makeWork(arrOfArr,worker) {
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++){
+    let arrSum = worker(arrOfArr[i]); //сохраняем переменную sum, возвращенную ф-цией worker
+    if (arrSum > max) {
+      max = arrSum;
+    }
+  }
+   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = -Infinity;
+  let min = Infinity;
+  for (let i = 0; i < arr.length; i++){ //перебираем массив, находим макс и мин
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  let difference = Math.abs(max - min);
+  return difference;
 }
